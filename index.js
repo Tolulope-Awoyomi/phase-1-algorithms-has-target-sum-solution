9873e0a934531b00fa994be02bbb2b08c42dde18
+ <+>
2022-12-29 22:08:18 -0500

    commit tree 1e56bb9e433ac038c68bd58a84bda4cd5b171dee

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+ function hasTargetSum(array, target) {
+    // create an object to keep track of all the numbers we've seen
+    const seenNumbers = {};
+    // iterate over the array of numbers
+    for (const number of array) {
+      // for the current number, identify a complementary number that adds to our target
+      // (for example: if our number is 2, and the target is 5, the complementary number is 3)
+      const complement = target - number;
+      // check if any of the keys in our object is the complement to the current number
+      // if so, return true
+      if (seenNumbers[complement]) return true;
+      // save the current number as the key on our object so we can check it later against other numbers
+      seenNumbers[number] = true;
+    }
+    // if we reach the end of the array, return false
+    return false;
+  }
+
 
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+create an object to keep track of all the numbers we've seen
+iterate over the array of numbers
+  for the current number, identify a complementary number that adds to our target
+  (for example: if our number is 2, and the target is 5, the complementary number is 3)
+  check if any of the keys in our object is the complement to the current number
+    if so, return true
+  save the current number as the key on our object so we can check it later against other numbers
+if we reach the end of the array, return false
+
 */
 
 /*
@@ -16,7 +40,6 @@ function hasTargetSum(array, target) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
@@ -29,6 +52,23 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("");
+  // Negative numbers?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+  // Multiple pairs?
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+  // Single numbers?
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 4));
 }
 
 module.exports = hasTargetSum;
